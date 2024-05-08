1b906a1927a776e01023f4d2240debaf560fa3d5
AndreiZachi <andreizachi@gmail.com>
2024-05-08 13:26:36 +0100

    v1.0
    First Commit

---
+++ MineChess/Models/GameState.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MineChess.Models
+{
+    class GameState
+    {
+        public bool isWon = false;
+        public bool isLost = false;
+        public bool HasEnded { get => (isWon || isLost); }
+    }
+
+}
---
+++ MineChess/Models/Square.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MineChess.Models
+{
+    class Square
+    {
+        public int Value { get; set; } = 0;
+        public bool IsBomb { get; set; } = false;
+        public string StringValue { get; set; }
+
+    }
+}
